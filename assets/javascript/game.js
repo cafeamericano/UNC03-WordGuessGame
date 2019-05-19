67f479778354c90622e77f930ed41dc56cef9c3a
Matthew <mfarmer5102@gmail.com>
2019-05-19 19:41:39 -0400

    Cleaning up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,12 @@
+//**************************************************GLOBAL VARIABLES**************************************************
+
+let winningLetters = [];
+let rightEntries = [];
+let wrongEntries = [];
+let numberOfGamesWon = 0;
+
+//**************************************************OBJECTS**************************************************
+
 let gameSession = {
   guessesRemaining: 20,
   wordPool: ["Mario", "Luigi", "Yoshi", "Bowser"]
@@ -6,22 +15,49 @@ let gameSession = {
 let randomIndex = Math.floor(Math.random() * gameSession.wordPool.length);
 let winningWord = gameSession.wordPool[randomIndex];
 
-document.getElementById("winningWord").innerHTML = winningWord
+document.getElementById("winningWord").innerHTML = winningWord;
 
-let winningLetters = [];
 
-let rightEntries = [];
-let wrongEntries = [];
-
-let numberOfGamesWon = 0;
 
 //Define what letters are correct
 for (i = 0; i < winningWord.length; i++) {
   winningLetters.push(winningWord[i]);
 }
 
-//Accept user input
+//**************************************************FUNCTIONS**************************************************
+function updateScoreboard() {
+  document.getElementById("rightEntries").innerHTML = rightEntries;
+  document.getElementById("wrongEntries").innerHTML = wrongEntries;
+  document.getElementById("guessesRemaining").innerHTML =
+    gameSession.guessesRemaining;
+  document.getElementById("gamesWon").innerHTML = numberOfGamesWon;
+}
+
+function checkForWin() {
+  //For every winning letter
+  let neededToWin = winningLetters.length;
+  let gotRight = 0;
+  for (i = 0; i < winningLetters.length; i++) {
+    //If it exists in rightEntries
+    if (rightEntries.indexOf(winningLetters[i]) !== -1) {
+      //Increase the number of letters gotten right by one
+      gotRight = gotRight + 1;
+    }
+  }
+  //If the number of letters gotten right is equal to that needed to win, player wins
+  if (gotRight === neededToWin) {
+    alert("You won!");
+    numberOfGamesWon += 1;
+    updateScoreboard();
+  }
+  console.log("Got right: " + gotRight);
+}
+
+//**************************************************RUNTIME**************************************************
+
+//Handle user input
 document.onkeyup = function(event) {
+  //Define key entered as 'x'
   let x = event.key;
   //If a losing letter that hasn't been entered before, penalize
   if (winningLetters.indexOf(x) === -1 && wrongEntries.indexOf(x) === -1) {
@@ -35,38 +71,7 @@ document.onkeyup = function(event) {
     rightEntries.push(x);
     console.log("Correct letter entered!");
   }
-  console.log("===============================");
-  console.log("Right entries: " + rightEntries);
-  console.log("Wrong entries: " + wrongEntries);
-  console.log("Remaining guesses: " + gameSession.guessesRemaining);
 
   updateScoreboard();
   checkForWin();
 };
-
-function updateScoreboard() {
-    document.getElementById("rightEntries").innerHTML = rightEntries;
-    document.getElementById("wrongEntries").innerHTML = wrongEntries;
-    document.getElementById("guessesRemaining").innerHTML = gameSession.guessesRemaining;
-    document.getElementById("gamesWon").innerHTML = numberOfGamesWon;
-}
-
-function checkForWin() {
-    //For every winning letter
-    let neededToWin = winningLetters.length;
-    let gotRight = 0;
-    for(i=0; i < winningLetters.length; i++) {
-        //If it exists in rightEntries
-        if (rightEntries.indexOf(winningLetters[i]) !== -1) {
-            //Increase the number of letters gotten right by one
-            gotRight = gotRight +1
-        }
-    }
-    //If the number of letters gotten right is equal to that needed to win, player wins
-    if(gotRight === neededToWin) {
-        alert('You won!')
-        numberOfGamesWon += 1;
-        updateScoreboard();
-    }
-    console.log("Got right: " + gotRight)
-}
