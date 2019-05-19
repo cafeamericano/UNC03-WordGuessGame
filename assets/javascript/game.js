8fe6a3765aa257ce3c293c0bb72eb88eef75b074
Matthew <mfarmer5102@gmail.com>
2019-05-19 19:54:45 -0400

    Successfully implemented case insensitivity

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,72 @@
-//**************************************************GLOBAL VARIABLES**************************************************
+//**************************************************INITIALIZE GLOBAL VARIABLES**************************************************
 
+let wordPool = ["Mario", "Luigi", "Yoshi", "Bowser"];
+let guessesRemaining = 0;
+
+let numberOfGamesWon = 0;
+let randomIndex = 0;
+let winningWord = "";
 let winningLetters = [];
 let rightEntries = [];
 let wrongEntries = [];
-let numberOfGamesWon = 0;
 
 //**************************************************OBJECTS**************************************************
 
-let gameSession = {
-  guessesRemaining: 20,
-  wordPool: ["Mario", "Luigi", "Yoshi", "Bowser"]
-};
+//**************************************************FUNCTIONS**************************************************
 
-let randomIndex = Math.floor(Math.random() * gameSession.wordPool.length);
-let winningWord = gameSession.wordPool[randomIndex];
-
-document.getElementById("winningWord").innerHTML = winningWord;
-
-
-
-//Define what letters are correct
-for (i = 0; i < winningWord.length; i++) {
-  winningLetters.push(winningWord[i]);
+function beginNewGame() {
+  updateScoreboard(); //Reset
+  guessesRemaining = 20; //Reset
+  winningLetters, rightEntries, (wrongEntries = []); //Reset
+  randomIndex = Math.floor(Math.random() * wordPool.length);
+  winningWord = wordPool[randomIndex];
+  for (i = 0; i < winningWord.length; i++) {
+    winningLetters.push(winningWord[i].toLowerCase());
+  }
+  document.getElementById("winningWord").innerHTML = winningWord;
 }
 
-//**************************************************FUNCTIONS**************************************************
 function updateScoreboard() {
-  document.getElementById("rightEntries").innerHTML = rightEntries;
-  document.getElementById("wrongEntries").innerHTML = wrongEntries;
+  document.getElementById("rightEntries").innerHTML =
+    "Right entries: " + rightEntries;
+  document.getElementById("wrongEntries").innerHTML =
+    "Wrong entries: " + wrongEntries;
   document.getElementById("guessesRemaining").innerHTML =
-    gameSession.guessesRemaining;
-  document.getElementById("gamesWon").innerHTML = numberOfGamesWon;
+    "Guesses remaining: " + guessesRemaining;
+  document.getElementById("gamesWon").innerHTML =
+    "Number of games won: " + numberOfGamesWon;
 }
 
 function checkForWin() {
-  //For every winning letter
   let neededToWin = winningLetters.length;
   let gotRight = 0;
   for (i = 0; i < winningLetters.length; i++) {
-    //If it exists in rightEntries
+    //For every winning letter
     if (rightEntries.indexOf(winningLetters[i]) !== -1) {
-      //Increase the number of letters gotten right by one
-      gotRight = gotRight + 1;
+      //If it exists in rightEntries
+      gotRight = gotRight + 1; //Increase the number of letters gotten right by one
     }
   }
-  //If the number of letters gotten right is equal to that needed to win, player wins
   if (gotRight === neededToWin) {
+    //If the number of letters gotten right is equal to that needed to win, player wins
     alert("You won!");
     numberOfGamesWon += 1;
     updateScoreboard();
   }
-  console.log("Got right: " + gotRight);
 }
 
 //**************************************************RUNTIME**************************************************
 
+//Start the first game
+beginNewGame();
+
 //Handle user input
 document.onkeyup = function(event) {
   //Define key entered as 'x'
-  let x = event.key;
+  let x = event.key.toLowerCase();
   //If a losing letter that hasn't been entered before, penalize
   if (winningLetters.indexOf(x) === -1 && wrongEntries.indexOf(x) === -1) {
-    gameSession.guessesRemaining = gameSession.guessesRemaining - 1;
+    guessesRemaining = guessesRemaining - 1;
     wrongEntries.push(x);
     //If a winning letter that hasn't already been entered, reward
   } else if (
@@ -69,7 +74,6 @@ document.onkeyup = function(event) {
     rightEntries.indexOf(x) === -1
   ) {
     rightEntries.push(x);
-    console.log("Correct letter entered!");
   }
 
   updateScoreboard();
