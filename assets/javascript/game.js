f1e7cd0761f243a5a1eb26ff910bd596bdf33ada
Matthew <mfarmer5102@gmail.com>
2019-05-20 20:40:14 -0400

    Implemented objects and methods

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,117 +1,171 @@
 //**************************************************INITIALIZE GLOBAL VARIABLES**************************************************
 
-let wordPool = ["sword", "axe", "shield", "staff", "helmet", "gloves", "boots", "magic", "potion", "monster"];
-let defaultBeginGuesses = 5;
-
-let numberOfGamesWon = 0;
-let numberOfGamesLost = 0;
-
-let randomIndex = 0;
-let winningWord = "";
-
-let winningLetters = [];
-let concealedLetters = [];
-
-let rightEntries = [];
-let wrongEntries = [];
-
-let defaultEvilEyeXposition = "5%";
-let evilEyeXposition = 5;
-
-var battleTheme = document.getElementById("battleTheme");
-var gameOver = document.getElementById("gameOverSound");
-var fanfare = document.getElementById("fanfare");
+let wordPool = [
+  "sword",
+  "axe",
+  "shield",
+  "staff",
+  "helmet",
+  "gloves",
+  "boots",
+  "magic",
+  "potion",
+  "monster"
+];
 
 //**************************************************OBJECTS**************************************************
 
-//**************************************************FUNCTIONS**************************************************
-
-function beginNewGame() {
-  fanfare.pause();
-  fanfare.currentTime = 0;
-  gameOver.pause();
-  gameOver.currentTime = 0;
-  guessesRemaining = defaultBeginGuesses; //Reset
-  winningLetters = []; //Reset
-  concealedLetters = []; //Reset
-  rightEntries = []; //Reset
-  wrongEntries = []; //Reset
-
-  randomIndex = Math.floor(Math.random() * wordPool.length);
-  winningWord = wordPool[randomIndex];
-  for (i = 0; i < winningWord.length; i++) {
-    winningLetters.push(winningWord[i].toLowerCase());
-  }
-
-  //Display anonymized letters
-  for (i = 0; i < winningWord.length; i++) {
-    concealedLetters.push("__  ");
-  }
-
-  updateScoreboard(); //Reset
-  document.getElementById("princess").style.transform = "scale(3)";
-  document.getElementById("evilEye").style.left = defaultEvilEyeXposition;
-  evilEyeXposition = 5;
-
-  battleTheme.play();
-}
-
-function updateScoreboard() {
-  document.getElementById("rightEntries").innerHTML =
-    "Right entries: " + rightEntries;
-  document.getElementById("wrongEntries").innerHTML =
-    "Wrong entries: " + wrongEntries;
-  document.getElementById("guessesRemaining").innerHTML =
-    "Guesses remaining: " + guessesRemaining;
-  document.getElementById("gamesWon").innerHTML =
-    "Number of games won: " + numberOfGamesWon;
-  document.getElementById("gamesLost").innerHTML =
-    "Number of games lost: " + numberOfGamesLost;
-  document.getElementById("concealedLetters").innerHTML =
-    "Concealed letters: " + concealedLetters.join(" ");
-}
-
-function checkForWin() {
-  let neededToWin = winningLetters.length;
-  let gotRight = 0;
-  for (i = 0; i < winningLetters.length; i++) {
-    //For every winning letter
-    if (rightEntries.indexOf(winningLetters[i]) !== -1) {
-      //If it exists in rightEntries
-      gotRight = gotRight + 1; //Increase the number of letters gotten right by one
+//Magic Word//////////////////////////////////////////////////
+let magicWord = {
+  randomIndex: 0,
+  winningWord: '',
+  winningLetters: [],
+  concealedLetters: [],
+  rightEntries: [],
+  wrongEntries: [],
+  clear: function() {
+    this.winningLetters = []; //Reset
+    this.concealedLetters = []; //Reset
+    this.rightEntries = []; //Reset
+    this.wrongEntries = []; //Reset
+  },
+  choose: function() {
+    this.randomIndex = Math.floor(Math.random() * wordPool.length);
+    this.winningWord = wordPool[this.randomIndex];
+    for (i = 0; i < this.winningWord.length; i++) {
+      this.winningLetters.push(this.winningWord[i].toLowerCase());
     }
-  }
-  if (gotRight === neededToWin) {
-    //If the number of letters gotten right is equal to that needed to win, player wins
-    battleTheme.pause();
-    fanfare.play();
-    numberOfGamesWon += 1;
-    updateScoreboard();
-    setTimeout(function() {
-      beginNewGame();
-    }, 5000);
-  }
+    //Display anonymized letters
+    for (i = 0; i < this.winningWord.length; i++) {
+      this.concealedLetters.push("__  ");
+    }
+  },
+
 }
 
-function checkForLoss() {
-  if (guessesRemaining === 0) {
-    battleTheme.pause();
-    gameOver.play();
-    document.getElementById("princess").style.transform =
-      "rotate(90deg) scale(3)";
-    numberOfGamesLost += 1;
-    setTimeout(function() {
-      beginNewGame();
-    }, 6000);
+//Game//////////////////////////////////////////////////
+let game = {
+  defaultBeginGuesses: 5,
+  guessesRemaining: 0,
+  numberOfGamesWon: 0,
+  numberOfGamesLost: 0,
+
+  resetGuessCount: function() {
+    this.guessesRemaining = this.defaultBeginGuesses; //Reset
+  },
+  
+  beginNew: function() {
+    this.resetGuessCount();
+    audio.reset();
+    magicWord.clear();
+    magicWord.choose();
+    scoreboard.update();
+    princess.resetSprite();
+    enemy.resetStartPosition();
+    audio.battleTheme.play();
   }
-}
+};
+
+//Status//////////////////////////////////////////////////
+let status = {
+  checkForLoss: function() {
+    if (game.guessesRemaining === 0) {
+      audio.battleTheme.pause();
+      audio.gameOver.play();
+      princess.makeDead();
+      game.numberOfGamesLost += 1;
+      setTimeout(function() {
+        game.beginNew();
+      }, 6000);
+    }
+  },
+  checkForWin: function() {
+    let neededToWin = magicWord.winningLetters.length;
+    let gotRight = 0;
+    for (i = 0; i < magicWord.winningLetters.length; i++) {
+      //For every winning letter
+      if (magicWord.rightEntries.indexOf(magicWord.winningLetters[i]) !== -1) {
+        //If it exists in rightEntries
+        gotRight = gotRight + 1; //Increase the number of letters gotten right by one
+      }
+    }
+    if (gotRight === neededToWin) {
+      //If the number of letters gotten right is equal to that needed to win, player wins
+      audio.battleTheme.pause();
+      audio.fanfare.play();
+      game.numberOfGamesWon += 1;
+      scoreboard.update();
+      setTimeout(function() {
+        game.beginNew();
+      }, 5000);
+    }
+  },
+  check: function() {
+    this.checkForWin();
+    this.checkForLoss();
+  }
+};
+
+//Scoreboard//////////////////////////////////////////////////
+let scoreboard = {
+  update: function() {
+    document.getElementById("rightEntries").innerHTML = "Right entries: " + magicWord.rightEntries;
+    document.getElementById("wrongEntries").innerHTML = "Wrong entries: " + magicWord.wrongEntries;
+    document.getElementById("guessesRemaining").innerHTML = "Guesses remaining: " + game.guessesRemaining;
+    document.getElementById("gamesWon").innerHTML = "Number of games won: " + game.numberOfGamesWon;
+    document.getElementById("gamesLost").innerHTML = "Number of games lost: " + game.numberOfGamesLost;
+    document.getElementById("concealedLetters").innerHTML = "Concealed letters: " + magicWord.concealedLetters.join(" ");
+  }
+};
+
+//Enemy//////////////////////////////////////////////////
+let enemy = {
+  defaultXposition: 50,
+  xPosition: 5,
+  moveRight: function() {
+    this.xPosition += 100;
+    document.getElementById("evilEye").style.left = this.xPosition + "px";
+    console.log(this.xPosition)
+  },
+  resetStartPosition: function() {
+    this.xPosition = this.defaultXposition;
+    document.getElementById("evilEye").style.left = this.defaultXposition + "px";
+  }
+};
+
+//Princess//////////////////////////////////////////////////
+let princess = {
+  makeDead: function() {
+    document.getElementById("princess").style.transform = "rotate(90deg) scale(3)";
+  },
+  resetSprite: function() {
+    document.getElementById("princess").style.transform = "scale(3)";
+  }
+};
+
+//Audio//////////////////////////////////////////////////
+let audio = {
+  battleTheme: document.getElementById("battleTheme"),
+  gameOver: document.getElementById("gameOverSound"),
+  fanfare: document.getElementById("fanfare"),
+  reset: function() {
+    this.fanfare.pause();
+    this.fanfare.currentTime = 0;
+    this.gameOver.pause();
+    this.gameOver.currentTime = 0;
+    this.battleTheme.pause();
+    this.battleTheme.currentTime = 0;
+  }
+};
 
 //**************************************************RUNTIME**************************************************
 
 //Start the first game
-setTimeout(function() {
-  beginNewGame();
-}, 0);
+let ready = confirm("Ready to play?")
+if(ready){
+  audio.reset();
+  game.beginNew();
+}
 
 //Handle user input
 document.onkeyup = function(event) {
@@ -131,28 +185,26 @@ document.onkeyup = function(event) {
     x = event.key.toLowerCase();
   }
   //If a losing letter that hasn't been entered before, penalize
-  if (winningLetters.indexOf(x) === -1 && wrongEntries.indexOf(x) === -1) {
-    guessesRemaining = guessesRemaining - 1;
-    wrongEntries.push(x);
-
-    evilEyeXposition += 100 / defaultBeginGuesses - 4.3;
-    document.getElementById("evilEye").style.left = evilEyeXposition + "%";
+  if (magicWord.winningLetters.indexOf(x) === -1 && magicWord.wrongEntries.indexOf(x) === -1) {
+    game.guessesRemaining = game.guessesRemaining - 1;
+    magicWord.wrongEntries.push(x);
+    enemy.moveRight();
+    
 
     //If a winning letter that hasn't already been entered, reward
   } else if (
-    winningLetters.indexOf(x) !== -1 &&
-    rightEntries.indexOf(x) === -1
+    magicWord.winningLetters.indexOf(x) !== -1 &&
+    magicWord.rightEntries.indexOf(x) === -1
   ) {
-    rightEntries.push(x);
+    magicWord.rightEntries.push(x);
 
-    for (i = 0; i < winningLetters.length; i++) {
-      if (x === winningLetters[i]) {
-        concealedLetters[i] = winningLetters[i];
+    for (i = 0; i < magicWord.winningLetters.length; i++) {
+      if (x === magicWord.winningLetters[i]) {
+        magicWord.concealedLetters[i] = magicWord.winningLetters[i];
       }
     }
   }
 
-  checkForWin();
-  updateScoreboard();
-  checkForLoss();
+  status.check();
+  scoreboard.update();
 };
